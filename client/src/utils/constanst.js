22bd725cbb3b5718b857799c7dfbf813e258a95f
nquanna <quanhm153@gmail.com>
2025-07-21 10:27:36 +0700

    prepqre  deploy to railway

--- client/src/utils/constanst.js
+++ client/src/utils/constanst.js
@@ -2,7 +2,7 @@ const constanst = {
   baseUrl:
     process.env.NODE_ENV === "development"
       ? process.env.REACT_APP_BASE_URL_DEV
-      : "https://firstapp-pzi0.onrender.com",
+      : process.env.REACT_APP_BASE_URL_PROD,
   otpDelay: 10,
 };
 
