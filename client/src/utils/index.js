90df4a50f64e7098fa1aafc727af745b9cb9e4ba
nquanna <quanhm153@gmail.com>
2025-08-02 16:25:14 +0700

    get micro - done

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,2 @@
 export { default as api } from "./api";
 export { default as constanst } from "./constanst";
-export { default as recognition } from "./recognition";
-export * from "./recognition";
-export { default as audio } from "./audio";
-export { default as speech } from "./speech";
